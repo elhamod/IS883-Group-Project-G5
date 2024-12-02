30e3d6c7c617796b49f2a205a437ca9581309ff5
rjcontrerasr <156374431+rjcontrerasr@users.noreply.github.com>
2024-12-01 19:19:29 -0500

    Chat_code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,67 @@
 import streamlit as st
+from langchain.memory import ConversationBufferWindowMemory
+from langchain_openai import ChatOpenAI
+from langchain.agents import AgentExecutor, create_tool_calling_agent, create_react_agent
+from langchain import hub
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Show title and description.
+st.title("💬 Chatbot")
+
+### Important part.
+# Create a session state variable to flag whether the app has been initialized.
+# This code will only be run first time the app is loaded.
+if "memory" not in st.session_state: ### IMPORTANT.
+    model_type="gpt-4o-mini"
+
+    # initialize the momory
+    max_number_of_exchanges = 10
+    st.session_state.memory = ConversationBufferWindowMemory(memory_key="chat_history", k=max_number_of_exchanges, return_messages=True) ### IMPORTANT to use st.session_state.memory.
+
+    # LLM
+    chat = ChatOpenAI(openai_api_key=st.secrets["OpenAI_API_KEY"], model=model_type)
+
+    # tools
+    from langchain.agents import tool
+    from datetime import date
+    @tool
+    def datetoday(dummy: str) -> str:
+        """Returns today's date, use this for any \
+        questions that need today's date to be answered. \
+        This tool returns a string with today's date.""" #This is the desciption the agent uses to determine whether to use the time tool.
+        return "Today is " + str(date.today())
+
+    tools = [datetoday]
+    
+    # Now we add the memory object to the agent executor
+    # prompt = hub.pull("hwchase17/react-chat")
+    # agent = create_react_agent(chat, tools, prompt)
+    from langchain_core.prompts import ChatPromptTemplate
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a helpful assistant."),
+            ("placeholder", "{chat_history}"),
+            ("human", "{input}"),
+            ("placeholder", "{agent_scratchpad}"),
+        ]
+    )
+    agent = create_tool_calling_agent(chat, tools, prompt)
+    st.session_state.agent_executor = AgentExecutor(agent=agent, tools=tools,  memory=st.session_state.memory, verbose= True)  # ### IMPORTANT to use st.session_state.memory and st.session_state.agent_executor.
+
+# Display the existing chat messages via `st.chat_message`.
+for message in st.session_state.memory.buffer:
+    # if (message.type in ["ai", "human"]):
+    st.chat_message(message.type).write(message.content)
+
+# Create a chat input field to allow the user to enter a message. This will display
+# automatically at the bottom of the page.
+if prompt := st.chat_input("What is up?"):
+    
+    # question
+    st.chat_message("user").write(prompt)
+
+    # Generate a response using the OpenAI API.
+    response = st.session_state.agent_executor.invoke({"input":prompt})['output']
+
+    # response
+    st.chat_message("assistant").write(response)
+    # st.write(st.session_state.memory.buffer)
